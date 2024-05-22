3f4d98c1685e68135f47f5789884b04e0f6bfd6b
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-05-22 22:25:47 +0530

    Added font Size For ABout Us Page

--- aboutus copy/script.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener("scroll", function () {
-    var nav = document.querySelector("nav");
-    if (window.pageYOffset > window.innerHeight / 2) {
-        nav.style.backgroundColor = "#1953c7";
-    } else {
-        nav.style.backgroundColor = "transparent";
-    }
-});
